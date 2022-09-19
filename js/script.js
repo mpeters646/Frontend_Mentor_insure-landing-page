93bb2b4b525501bda087e6eca6c0822c3534fb69
Marco Peters <m.peters646@gmail.com>
2022-09-19 18:55:24 +0200

    modified styles for mobile and added js for mobile nav

--- js/script.js
+++ js/script.js
@@ -2,12 +2,22 @@ const menuBtn = document.querySelector('#menuBtn');
 
 let toggle = true;
 
+function openNav() {
+  document.getElementById('navOverlay').style.display = 'block';
+}
+
+closeNav = () => {
+  document.getElementById('navOverlay').style.display = 'none';
+};
+
 menuBtn.addEventListener('click', () => {
   // toggle
   toggle = !toggle;
   if (toggle) {
     menuBtn.src = '../images/icon-hamburger.svg';
+    closeNav();
   } else {
     menuBtn.src = '../images/icon-close.svg';
+    openNav();
   }
 });
